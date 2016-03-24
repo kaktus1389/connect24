271decac49aeff2f600932fab1d53a332ed44feb
kalacanović aljoša <kalacanovicaljosa@gimvic.local>
2016-03-24 16:18:53 +0100

    added loop

--- startingfile.py
+++ startingfile.py
@@ -5,5 +5,11 @@ VISINA_EKRANA = 800
 
 def main():
 	ekran = pygame.display.set_mode([SIRINA_EKRANA,VISINA_EKRANA])
+	konec_zanke = False
+    ura = pygame.time.Clock()
+    while not konec_zanke:
+        ploscadi.draw(ekran)
+    pygame.quit()
+
 	ekran.fill((150,100,200))
 main()
