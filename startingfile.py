1e09e6c684d4d9f584d33bddebb33f227a316a3c
kalacanović aljoša <kalacanovicaljosa@gimvic.local>
2016-03-24 16:21:06 +0100

    indents fixed

--- startingfile.py
+++ startingfile.py
@@ -6,10 +6,10 @@ VISINA_EKRANA = 800
 def main():
 	ekran = pygame.display.set_mode([SIRINA_EKRANA,VISINA_EKRANA])
 	konec_zanke = False
-    ura = pygame.time.Clock()
-    while not konec_zanke:
-        ploscadi.draw(ekran)
-    pygame.quit()
+	ura = pygame.time.Clock()
+	while not konec_zanke:
+		ploscadi.draw(ekran)
+		pygame.quit()
 
 	ekran.fill((150,100,200))
 main()
