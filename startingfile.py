b0f36b5be757fa8cd023e28d222b83b84dfaed96
kalacanović aljoša <kalacanovicaljosa@gimvic.local>
2016-03-24 16:29:19 +0100

    loop

--- startingfile.py
+++ startingfile.py
@@ -6,7 +6,11 @@ VISINA_EKRANA = 800
 def main():
 	ekran = pygame.display.set_mode(
 		[SIRINA_EKRANA,VISINA_EKRANA])
-	ekran.fill((150,100,200))
-	pygame.display.flip()
-	
+	ura = pygame.time.Clock()
+	konec_zanke = False
+	while not konec_zanke:
+		ura.tick(60)
+		ekran.fill((150,100,200))
+		pygame.display.flip()
+	pygame.quit()
 main()
