8eb58038816d23021c4181508a69db539063f22b
kalacanović aljoša <kalacanovicaljosa@gimvic.local>
2016-03-24 16:40:01 +0100

    more added

--- startingfile.py
+++ startingfile.py
@@ -3,9 +3,23 @@ import pygame
 SIRINA_EKRANA = 600
 VISINA_EKRANA = 800
 
+class Interface(pygame.sprite.Sprite):
+	def __init__(self,zetoni = None):
+		super().__init()
+		self.zetoni = zetoni
+		sirina = 150
+		visina = 300
+		self.image = pygame.Surface((sirina,visina))
+		self.image.fill((255,0,0))
+		self.rect = self.image.get_rect()
+
+
 def main():
 	ekran = pygame.display.set_mode(
 		[SIRINA_EKRANA,VISINA_EKRANA])
+	group = pygame.sprite.Group()
+	ui = Interface()
+	skupina.add(ui)
 	ura = pygame.time.Clock()
 	konec_zanke = False
 	while not konec_zanke:
@@ -13,7 +27,8 @@ def main():
 		for dogodek in pygame.event.get():
 			if dogodek.type == pygame.QUIT:
 				konec_zanke = True
-		ekran.fill((150,100,200))
+		ekran.fill((200,50,50))
 		pygame.display.flip()
+	skupina.draw(ekran)
 	pygame.quit()
 main()
