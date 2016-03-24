30b951d12193ef377e76290f105f8e4a6f6a1643
kalacanović aljoša <kalacanovicaljosa@gimvic.local>
2016-03-24 16:24:26 +0100

    fixed

--- startingfile.py
+++ startingfile.py
@@ -4,12 +4,9 @@ SIRINA_EKRANA = 600
 VISINA_EKRANA = 800
 
 def main():
-	ekran = pygame.display.set_mode([SIRINA_EKRANA,VISINA_EKRANA])
-	konec_zanke = False
-	ura = pygame.time.Clock()
-	while not konec_zanke:
-		ploscadi.draw(ekran)
-		pygame.quit()
-
+	ekran = pygame.display.set_mode(
+		[SIRINA_EKRANA,VISINA_EKRANA])
 	ekran.fill((150,100,200))
+	pygame.display.flip()
+	
 main()
